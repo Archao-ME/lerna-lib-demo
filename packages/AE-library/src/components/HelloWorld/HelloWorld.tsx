9f68b43681f51d61317cea16f41937eda0ed1d8a
jichaochen <brpoper@gmail.com>
2021-07-31 16:32:55 +0800

    hammer: 添加prettier和eslint

--- packages/AE-library/src/components/HelloWorld/HelloWorld.tsx
+++ packages/AE-library/src/components/HelloWorld/HelloWorld.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
+import * as React from "react";
 
-const HelloWorld = () => {
-    return <div>Hello World!</div>
-}
+const HelloWorld: React.FC = () => {
+  return <div>Hello World!</div>;
+};
 
-export default HelloWorld
+export default HelloWorld;
--- packages/AE-library/src/components/HelloWorld/HellowWorld.stories.tsx
+++ packages/AE-library/src/components/HelloWorld/HellowWorld.stories.tsx
@@ -3,9 +3,8 @@ import { ComponentMeta } from "@storybook/react";
 import * as React from "react";
 
 export default {
-    title: 'Components/HelloWorld',
-    component: HelloWorld
-} as ComponentMeta<typeof HelloWorld>
+  title: "Components/HelloWorld",
+  component: HelloWorld,
+} as ComponentMeta<typeof HelloWorld>;
 
-export const FirstWorld = () => <HelloWorld />
-
+export const FirstWorld = () => <HelloWorld />;
