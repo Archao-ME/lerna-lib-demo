a7a9c33b19246d1ae38d903a6a8dc68b4fe7e430
jichaochen <brpoper@gmail.com>
2021-07-31 15:06:24 +0800

    Init react & typescript & storybook

---
+++ packages/AE-library/src/components/HelloWorld/HellowWorld.stories.tsx
@@ -0,0 +1,11 @@
+import { HelloWorld } from "./HelloWorld";
+import { ComponentMeta } from "@storybook/react";
+import * as React from "react";
+
+export default {
+    title: 'Components/HelloWorld',
+    component: HelloWorld
+} as ComponentMeta<typeof HelloWorld>
+
+export const FirstWorld = () => <HelloWorld />
+
